2cf056095306fdcf80bbaf440f13a483a69e7a17
Sam Markham <sam.e.markham@gmail.com>
2020-08-14 16:04:47 -0700

    Adds Generator component with filler code

---
+++ src/components/Generator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Generator = () => {
+  return (
+    <div>
+      <p>Generator here</p>
+      <button>Generate</button>
+    </div>
+  );
+}
+
+export default Generator;
--- src/components/PromptPage.js
+++ src/components/PromptPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Generator from './Generator';
 
 const PromptPage = () => {
   return (
     <div>
       <h1>Prompt Generator</h1>
-
+      <Generator />
     </div>
   );
 }
