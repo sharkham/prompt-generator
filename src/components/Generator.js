295775953a0b5169ee9a5d21d52f4a8c2bfe8f4e
Sam Markham <sam.e.markham@gmail.com>
2020-08-14 16:37:33 -0700

    Adds commented out code for generating random item from JavaScript array

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -3,12 +3,13 @@ import React from 'react';
 const Generator = () => {
 
   const generate = () => {
+    // const randomElement = array[Math.floor(Math.random() * array.length)];
     console.log("generate function firing")
   }
 
   return (
     <div>
-      <p>Generator here</p>
+      <p>Prompt here</p>
       <button onClick={generate}>Generate</button>
     </div>
   );
