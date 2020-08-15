1dd313c7492a73f57b6c11ba276f56b45773440d
Sam Markham <sam.e.markham@gmail.com>
2020-08-15 13:20:37 -0700

    Adds routes to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PromptPage from './components/PromptPage'
+import {
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <PromptPage/>
+      <Switch>
+        <Route exact path="/"><PromptPage/></Route>
+        <Route exact path="/words"><PromptPage/></Route>
+      </Switch>
     </div>
   );
 }
