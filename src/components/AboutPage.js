88ddd790bdf03a38c7e32f83787d20cc1080a62d
Sam Markham <sam.e.markham@gmail.com>
2020-08-15 13:48:05 -0700

    Fills out AboutPage

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -3,7 +3,8 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <div>
-      <p>This is an About page.</p>
+      <p>This is a writing prompt generator built with React. Check out the code on GitHub <a target="blank" href="https://github.com/sharkham/prompt-generator">here!</a></p>
+      <p>Want to get in touch? You can find my contact info <a target="blank" href="https://github.com/sharkham">here.</a></p>
     </div>
   );
 }
