09b356d68fb961873942266a7589b1cb01fcba4f
Sam Markham <sam.e.markham@gmail.com>
2020-08-15 13:40:32 -0700

    Cleans up Generator

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -1,23 +1,13 @@
 import React, { useEffect, useState } from 'react';
-// import {words} from '../assets/words';
 
 const Generator = (props) => {
-
-  // useEffect(() => {
-
-  // })
 
   const [prompt, setPrompt] = useState("Click to generate a prompt!")
 
 
 
   const generate = () => {
-    // const randomElement = array[Math.floor(Math.random() * array.length)];
-    // console.log(words[Math.floor(Math.random() * words.length)])
-    let selectedPrompt = props.dataset[Math.floor(Math.random() * props.dataset.length)];
-    console.log(selectedPrompt)
-    // currently logs two prompts instead of one??
-    return selectedPrompt;
+    return props.dataset[Math.floor(Math.random() * props.dataset.length)];
   }
 
   return (
