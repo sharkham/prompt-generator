027d83d83002e9ffb1d8dab0bd931c04b3058e92
Sam Markham <sam.e.markham@gmail.com>
2020-08-15 13:30:32 -0700

    Adds NavBar links

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import PromptPage from './components/PromptPage'
 import {
   Switch,
-  Route,
-  BrowserRouter as Router
+  Route
 } from 'react-router-dom';
 import NavBar from './components/NavBar';
 
@@ -12,12 +11,10 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Router>
         <Switch>
           <Route exact path="/"><PromptPage/></Route>
           <Route exact path="/words"><PromptPage/></Route>
         </Switch>
-      </Router>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <div>
-      <p>Nav Bar</p>
+      <Link to="/">Prompts</Link>
+      <Link to="/words">More Prompts</Link>
+      <Link to="/about">About</Link>
     </div>
   );
 }
