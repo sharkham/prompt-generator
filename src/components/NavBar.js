06c66d7057aa405970e3fb319c7dcde68faa1050
Sam Markham <sam.e.markham@gmail.com>
2020-08-18 11:35:08 -0700

    Adjusts styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,17 +4,9 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <nav role="navigation">
-      <ul className="menu">
-        <li>
           <Link className="nav-link" to="/">Words</Link>
-        </li>
-        <li>
           <Link className="nav-link" to="/nauticalwords">Nautical Words</Link>
-        </li>
-        <li>
           <Link className="nav-link" to="/about">About</Link>
-        </li>
-      </ul>
     </nav>
   );
 }
