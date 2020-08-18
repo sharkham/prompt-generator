92d98cc53fcd4a32ada40ba4a8a3cd22ee63b406
Sam Markham <sam.e.markham@gmail.com>
2020-08-17 18:51:09 -0700

    Adds more styling and removes emojis from NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      <Link to="/"><span role="img" aria-label="Words">📗</span></Link>
-      <Link to="/nauticalwords"><span role="img" aria-label="Nautical Words">⚓️</span></Link>
-      <Link to="/about">About</Link>
+      <Link className="nav-link" to="/">Words</Link>
+      <Link className="nav-link" to="/nauticalwords">Nautical Words</Link>
+      <Link className="nav-link" to="/about">About</Link>
     </div>
   );
 }
